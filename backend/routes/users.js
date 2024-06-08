e60ed733704738c5c2682c98b092f369dd1eed21
Krishan <117340271+shoonya0@users.noreply.github.com>
2024-06-09 00:00:37 +0530

    -created an delete request with tweets data model.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const createError = require('http-errors');
 const handleError = require('../middleware/error');
 const User = require('../dataModels/User');
+const Tweet = require('../dataModels/Tweets');
+
 
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
@@ -22,15 +24,19 @@ router.get("/find/:id", async (req, res, next) => {
 
 
 // Update User
-router.put("/:id", async (req, res, next) => {
+router.put("/:id", verifyToken ,async (req, res, next) => {
     if(req.params.id === req.body.id){
         try{
+            // here we are updating the user with the help of id and setting the new data to the user
             const updateUser = await User.findByIdAndUpdate(
+                // here we are finding the user by id
                 req.params.id,
                 {
+                    // here we are setting the new data to the user
                     $set: req.body,
                 },
                 {
+                    // we only want to attach the new data to the user by omitting the old data
                     new: true
                 }
             );
@@ -39,10 +45,27 @@ router.put("/:id", async (req, res, next) => {
             next(err);
         }
     }else{
-        return next(createError(401, "You can update only your account!"));
+        return next(createError(403, "You can update only your account!"));
     }
 });
 
+
+// Delete User
+router.delete("/:id", verifyToken, async (req, res, next) => {
+    if(req.params.id === req.body.id){
+        try{
+            // here we are deleting the user by id
+            await User.findByIdAndDelete(req.params.id);
+            // here we are deleting the tweets of the user by userId
+            await Tweet.deleteOne({userId: req.params.id});
+            res.status(200).json("User has been deleted...");
+        }catch(err){
+            next(err);
+        }
+    }else{
+        return next(handleError(403, "You can delete only your account!"));
+    }
+});
 
 
 module.exports = router;

b5ee38817953bccbf3cd509dd57d78b5babf984e
Krishan <117340271+shoonya0@users.noreply.github.com>
2024-06-08 15:14:11 +0530

    -Created a basic structure.

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

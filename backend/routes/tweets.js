d138f597c53007d04192c490d961d5a230addd5e
Krishan <117340271+shoonya0@users.noreply.github.com>
2024-06-09 15:32:27 +0530

    -created an post request to create tweet.

--- backend/routes/tweets.js
+++ backend/routes/tweets.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const verifyToken = require('../middleware/auth');
+const handleError = require('../middleware/error');
+const Tweet = require('../dataModels/Tweets');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send('Hello from users');
+// // Create a Tweet
+router.post("/", verifyToken, async (req , res , next) => {
+    const newTweet = new Tweet(req.body);
+    try{
+        const saveTweet = await newTweet.save();
+        res.status(200).json(saveTweet);
+    }catch(err){
+        handleError(err);
+    }
 });
-
 module.exports = router;
